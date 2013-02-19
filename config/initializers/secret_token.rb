9eaab2cf7e34e1c7e94575c742484f7a69eca2bf
masahiro aizawa <kingyodojo@gmail.com>
2013-02-19 23:32:07 +0900

    Generate new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MembersCurrency::Application.config.secret_token = 'e3dfe8cbb715ed7ab12bc3e7a2b759c13ba2e2a0e55c6f8576c935bfa75309327f70f1ee8d59afaf0b25bf71e7e86f109690f50a351f27452148ac6bc80ebd5c'
