cac2e9fee8a3c3a7ee6715463e14c162870afc1b
masahiro aizawa <kingyodojo@gmail.com>
2013-03-03 01:29:28 +0900

    Display login memebr name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def login_member_name
+    member_id = session[:login]
+    member_name = Member.find(:first, conditions: {member_id: member_id}).name
+    "Login: #{member_name}"
+  end
 end
